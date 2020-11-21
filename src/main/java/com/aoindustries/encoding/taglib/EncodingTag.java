9268558742497bca92696ca7fee1420e13df8830
AO Industries, Inc <support@aoindustries.com>
2020-11-21 17:36:42 -0600

    New tag <encoding:encoding> that has type defined in its type attribute.

---
+++ src/main/java/com/aoindustries/encoding/taglib/EncodingTag.java
@@ -0,0 +1,57 @@
+/*
+ * ao-encoding-taglib - High performance streaming character encoding in a JSP environment.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-encoding-taglib.
+ *
+ * ao-encoding-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-encoding-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-encoding-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.encoding.taglib;
+
+import com.aoindustries.encoding.MediaType;
+import com.aoindustries.lang.Strings;
+import java.io.UnsupportedEncodingException;
+import javax.servlet.jsp.JspTagException;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class EncodingTag extends EncodingFilteredTag {
+
+	@Override
+	public MediaType getContentType() {
+		return mediaType;
+	}
+
+/* BodyTag only:
+	private static final long serialVersionUID = 1L;
+/**/
+
+	private MediaType mediaType;
+	public void setType(String type) throws JspTagException {
+		String typeStr = Strings.trim(type);
+		MediaType newMediaType = MediaType.getMediaTypeByName(typeStr);
+		if(newMediaType==null) {
+			try {
+				newMediaType = MediaType.getMediaTypeForContentType(typeStr);
+			} catch(UnsupportedEncodingException e) {
+				throw new JspTagException(e);
+			}
+		}
+		this.mediaType = newMediaType;
+	}
+}
