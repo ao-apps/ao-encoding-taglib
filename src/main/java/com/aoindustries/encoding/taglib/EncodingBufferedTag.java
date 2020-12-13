dd233b5efb862cc3a576f363a1776dfde8ac610c
AO Industries, Inc <support@aoindustries.com>
2020-12-12 21:47:30 -0600

    UnsupportedEncodingException will not be needed

--- src/main/java/com/aoindustries/encoding/taglib/EncodingBufferedTag.java
+++ src/main/java/com/aoindustries/encoding/taglib/EncodingBufferedTag.java
@@ -120,14 +120,14 @@ public abstract class EncodingBufferedTag extends SimpleTagSupport {
 	/**
 	 * Gets the type of data that is contained by this tag.
 	 */
-	public abstract MediaType getContentType(); // TODO: Throws UnsupportedEncodingException? (See nmw-email-taglib/ContentTag.java)
+	public abstract MediaType getContentType();
 
 	/**
 	 * Gets the output type of this tag.  This is used to determine the correct
 	 * encoder.  If the tag never has any output this should return {@code null}.
 	 * When {@code null} is returned, any output will result in an error.
 	 */
-	public abstract MediaType getOutputType(); // TODO: Throws UnsupportedEncodingException?
+	public abstract MediaType getOutputType();
 
 	/**
 	 * Gets the number of characters that may be buffered before switching to the
